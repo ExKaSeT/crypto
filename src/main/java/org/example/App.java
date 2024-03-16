c1f34536f390a6b3d49fd46258766c1e4a1e785c
ExKaSeT <exkaset@mail.ru>
2024-03-16 19:11:06 +0300

    1. done

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.example.utils.Permutation;
+
+import java.util.Arrays;
+
+public class App  {
+    public static void main( String[] args ) {
+        byte[] arr = intToByteArray(123456789);
+        printByteArr(arr);
+        System.out.println(Arrays.toString(arr));
+        byte[] res = Permutation.permute(arr, new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17,18,19,20,21,22,23,24,25,26,27,28,29}, Permutation.DirectionRule.LEAST_TO_MOST, Permutation.IndexRule.FROM_0);
+        System.out.println(Arrays.toString(res));
+        printByteArr(res);
+    }
+
+    public static byte[] intToByteArray(int value) {
+        return new byte[] {
+                (byte)(value >>> 24),
+                (byte)(value >>> 16),
+                (byte)(value >>> 8),
+                (byte)value};
+    }
+    public static void printByteArr(byte[] arr) {
+        for (byte b : arr) {
+            for (int i = 7; i >= 0; i--) {
+                System.out.print((b >> i) & 1);
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+}
---
+++ src/main/java/org/example/utils/Permutation.java
@@ -0,0 +1,32 @@
+package org.example.utils;
+
+public class Permutation {
+    public enum DirectionRule {
+        LEAST_TO_MOST,
+        MOST_TO_LEAST,
+    }
+
+    public enum IndexRule {
+        FROM_0,
+        FROM_1,
+    }
+
+    public static byte[] permute(final byte[] data, final int[] rule, DirectionRule direction, IndexRule indexRule) {
+        int bitsLength = rule.length;
+        byte[] result = new byte[(int) Math.ceil(bitsLength / 8.0)];
+
+        for (int i = 0; i < bitsLength; i++) {
+            int index = indexRule == IndexRule.FROM_0 ? rule[i] : rule[i] - 1;
+            int byteIndex = index / 8;
+            int shiftCount = 7 - (index % 8);
+            byte bit = (byte) ((data[byteIndex] >> shiftCount) & 1);
+            if (direction == DirectionRule.LEAST_TO_MOST) {
+                result[i / 8] |= (byte) (bit << (7 - (i % 8)));
+            } else {
+                int reverseIndex = bitsLength - i - 1;
+                result[reverseIndex / 8] |= (byte) (bit << (7 - (reverseIndex % 8)));
+            }
+        }
+        return result;
+    }
+}
