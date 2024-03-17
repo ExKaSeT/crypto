581f9d4841701480447236be20734463f907ac9d
ExKaSeT <exkaset@mail.ru>
2024-03-17 18:39:52 +0300

    des round keys gen done

--- src/main/java/org/example/round_key/DesKeyGenerator.java
+++ src/main/java/org/example/round_key/DesKeyGenerator.java
@@ -29,6 +29,8 @@ public class DesKeyGenerator implements RoundKeyGenerator {
             throw new IllegalArgumentException("Incorrect key length");
         }
 
+        var keys = new byte[16][];
+
         var block1 = Permutation.permute(key, PC1_1, LEAST_TO_MOST, FROM_1);
         var block2 = Permutation.permute(key, PC1_2, LEAST_TO_MOST, FROM_1);
 
@@ -37,24 +39,40 @@ public class DesKeyGenerator implements RoundKeyGenerator {
             if (i == 1 || i == 2 || i == 9 || i == 16) {
                 shiftCount = 1;
             }
-
-
+            block1 = leftShift28bit(block1, shiftCount == 1);
+            block2 = leftShift28bit(block2, shiftCount == 1);
+            var block = merge28to56bit(block1, block2);
+            block = Permutation.permute(block, PC2, LEAST_TO_MOST, FROM_1);
+            keys[i - 1] = block;
         }
-
-        return new byte[0][];
+        return keys;
     }
 
-    public static byte[] leftShift28bit(final byte[] data, boolean isOneShift) {
+    private byte[] leftShift28bit(final byte[] data, boolean isOneShift) {
         var result = new byte[data.length];
-        int handler = isOneShift ? 1 : 3;
+        int handler = isOneShift ? 1 : 3; // ... 0/1 1
+        int handler1 = 15; // ... 0 0 0 0 1 1 1 1
         int swapBits = (data[0] >> (isOneShift ? 7 : 6)) & handler;
         int i = 0;
         for (; i < result.length - 1; i++) {
             result[i] = (byte) ((data[i] << (isOneShift ? 1 : 2)) | ((data[i + 1] >> (isOneShift ? 7 : 6) & handler)));
         }
         // 0 0 0 0 x x x/0 0 - where 'x' is data; x/0 - depends on shift count
-        byte lastByteShifted = (byte) ((data[i] << (isOneShift ? 1 : 2)) >> (4 + (isOneShift ? 1 : 2)) << (isOneShift ? 1 : 2));
+        byte lastByteShifted = (byte) (((data[i] << (isOneShift ? 1 : 2)) >> (4 + (isOneShift ? 1 : 2)) << (isOneShift ? 1 : 2)) & handler1);
         result[i] = (byte) ((lastByteShifted | swapBits) << 4);
+        return result;
+    }
+
+    private byte[] merge28to56bit(final byte[] block1, final byte[] block2) {
+        var result = new byte[7];
+        System.arraycopy(block1, 0, result, 0,4);
+        int handler1 = 240; // ... 1 1 1 1 0 0 0 0
+        result[3] = (byte) (result[3] | (byte) ((block2[0] & handler1) >> 4));
+        int handler = 15; // ... 0 0 0 0 1 1 1 1
+        for (int i = 4; i < result.length; i++) {
+            int block2Index = i - 4;
+            result[i] = (byte) (((block2[block2Index] & handler) << 4) | ((block2[block2Index + 1] & handler1) >> 4));
+        }
         return result;
     }
 }
